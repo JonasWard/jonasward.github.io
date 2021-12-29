2bba8cc963b7ac25c22bd44dab5e2dbc9dfba741
Jonas Ward Van den Bulcke <43600893+JonasWard@users.noreply.github.com>
2021-12-29 21:30:59 +0100

    react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 import {Game} from "./play/tik-tak-toe";
+import About from "./components/About";
 
 function App() {
-  return (
-    <div className="App">
-      <Game/>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About Us</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact Us</Link>
+                    </li>
+                </ul>
+                <Routes>
+                    <Route path="/" element={<Game/>}/>
+                    <Route path="about" element={<About/>}/>
+                    <Route path="contact" element={<h1>Contact Us</h1>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
