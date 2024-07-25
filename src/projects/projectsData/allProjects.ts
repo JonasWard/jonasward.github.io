69ca318c14eb872a92fb8559878ee859b6a7a4f8
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-07-25 17:53:52 +0200

    sorting all projects properly

--- src/projects/projectsData/allProjects.ts
+++ src/projects/projectsData/allProjects.ts
@@ -3,6 +3,10 @@ import { theCityIsArt } from './architecture/theCityIsArt';
 import { patternMachine } from './computationDesign/patternMachine';
 import { scarves } from './computationDesign/scarves';
 import { smartBrick } from './computationDesign/smartBrick';
+import { chocoladeChaud } from './design/chocoladeChaud';
+import { coralBricks } from './design/rrreefs';
 import { scarves2023 } from './design/scarves2023';
 
-export const allProjects = [patternMachine, livingOnTheEdge, theCityIsArt, scarves, smartBrick, scarves2023];
+export const allProjects = [patternMachine, livingOnTheEdge, theCityIsArt, scarves, smartBrick, coralBricks, scarves2023, chocoladeChaud].sort(
+  (a, b) => -a.id.localeCompare(b.id)
+);
