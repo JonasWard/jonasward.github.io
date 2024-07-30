6ff21995cf3deacac5117a14421b5d566f7841db
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-07-30 19:45:36 +0200

    improved project chip styling

--- src/projects/ProjectChip.tsx
+++ src/projects/ProjectChip.tsx
@@ -1,17 +1,18 @@
 import { getPrimaryColorForString, getSecondaryColorForString } from '../localisation/colorForString';
+import './ProjectChip.css';
 
 interface IProjectChipProps {
   name: string;
   size?: number;
 }
 
-export const ProjectChip: React.FC<IProjectChipProps> = ({ name, size = 22 }) => {
+export const ProjectChip: React.FC<IProjectChipProps> = ({ name, size = 24 }) => {
   return (
     <div
+      className='project-chip'
       style={{
         backgroundColor: getSecondaryColorForString(name),
         color: getPrimaryColorForString(name),
-        borderRadius: size / 2,
         display: 'inline-block',
         fontSize: size * 0.55,
         padding: '0 4px',
