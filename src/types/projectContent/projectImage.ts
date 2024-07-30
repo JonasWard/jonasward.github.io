6c43004408e63dbd7b226c03f27a1f786d4be0bf
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-07-30 19:44:23 +0200

    optional imageWidth & imageHeight

--- src/types/projectContent/projectImage.ts
+++ src/types/projectContent/projectImage.ts
@@ -3,6 +3,8 @@ import { ProjectContentType } from './projectContentType';
 export interface ProjectImage {
   type: ProjectContentType.Image;
   imageHref: string;
+  imageWidth?: number;
+  imageHeigth?: number;
   imageText?: string;
   imageTextSize?: 'small' | 'medium' | 'large';
   imageTextPosition?: 'top' | 'bottom' | 'center';
