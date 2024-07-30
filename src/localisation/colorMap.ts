012904a651521a35deb2a1b2d9a7667ef6f5fb09
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-07-30 19:43:41 +0200

    updated color maps

--- src/localisation/colorMap.ts
+++ src/localisation/colorMap.ts
@@ -3,47 +3,51 @@ import { NamedColor } from '../types/colorLocalisation';
 import { Keywords } from '../types/keywords';
 import { ProjectContext } from '../types/projectContext';
 import { ProjectPartnerContext } from '../types/projectPartnerContext';
+import { PLASMA } from './plasma';
+
+const getLightness = (color: [number, number, number]): number => 0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2];
+
+// helper method for getting a color for a given t value (between 0 and 1)
+const getColorFromUUIDPlasma = (t: number): [number, number, number] => {
+  const index = t * (PLASMA.length - 1);
+  const i0 = Math.floor(index);
+  const i1 = Math.ceil(index);
+
+  if (i0 === i1) return PLASMA[i0];
+  const f = index - i0;
+  return [PLASMA[i0][0] * (1 - f) + PLASMA[i1][0] * f, PLASMA[i0][1] * (1 - f) + PLASMA[i1][1] * f, PLASMA[i0][2] * (1 - f) + PLASMA[i1][2] * f];
+};
+
+const missingAttributes: (Keywords | ProjectContext | ProjectPartnerContext)[] = [
+  ...Object.values(Keywords),
+  ...Object.values(ProjectContext),
+  ...Object.values(ProjectPartnerContext),
+];
 
 export const PrimaryColorMap: Record<NamedColor, string> = {
   [ProjectCategory.Design]: 'white',
   [ProjectCategory.Architecture]: 'red',
   [ProjectCategory.Art]: 'white',
   [ProjectCategory.Urbanism]: 'red',
-  [Keywords.Development]: '',
-  [Keywords.Frontend]: 'darkblue',
-  [Keywords.Shaders]: 'yellow',
-  [Keywords.Geometry]: '',
-  [Keywords.ThreeDPrinting]: 'white',
-  [Keywords.Ecology]: 'white',
-  [Keywords.Knitting]: '',
-  [Keywords.Patterns]: '',
-  [ProjectContext.Academic]: 'blue',
-  [ProjectContext.Professional]: 'darkgreen',
-  [ProjectContext.Personal]: '',
-  [ProjectPartnerContext.Solo]: '',
-  [ProjectPartnerContext.Team]: '',
-  [ProjectPartnerContext.Client]: '',
-  [Keywords.DigitalFabrication]: '',
-};
+  ...Object.fromEntries(
+    missingAttributes.map((attributeName, index, array) => [
+      attributeName,
+      getLightness(getColorFromUUIDPlasma(index / (array.length - 1))) < 0.5 ? 'white' : 'black',
+    ])
+  ),
+} as Record<NamedColor, string>;
 
 export const SecondaryColorMap: Record<NamedColor, string> = {
   [ProjectCategory.Design]: 'red',
   [ProjectCategory.Architecture]: 'yellow',
   [ProjectCategory.Art]: 'blue',
   [ProjectCategory.Urbanism]: 'black',
-  [Keywords.Development]: '',
-  [Keywords.Frontend]: 'cyan',
-  [Keywords.Shaders]: 'purple',
-  [Keywords.Geometry]: '',
-  [Keywords.ThreeDPrinting]: 'gray',
-  [Keywords.Ecology]: 'darkgreen',
-  [Keywords.Knitting]: '',
-  [Keywords.Patterns]: '',
-  [ProjectContext.Academic]: 'ivory',
-  [ProjectContext.Professional]: 'lightgreen',
-  [ProjectContext.Personal]: '',
-  [ProjectPartnerContext.Solo]: '',
-  [ProjectPartnerContext.Team]: '',
-  [ProjectPartnerContext.Client]: '',
-  [Keywords.DigitalFabrication]: '',
-};
+  ...Object.fromEntries(
+    missingAttributes.map((attributeName, index, array) => [
+      attributeName,
+      `rgb(${getColorFromUUIDPlasma(index / (array.length - 1))
+        .map((v) => ((1 + v) % 1) * 255)
+        .join(',')})`,
+    ])
+  ),
+} as Record<NamedColor, string>;
