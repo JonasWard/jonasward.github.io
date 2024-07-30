d50c2cf6f45158c522362f67c8e31dec94e8a63c
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-07-30 23:09:24 +0200

    colorMap updated with viridis for technologies

--- src/localisation/colorMap.ts
+++ src/localisation/colorMap.ts
@@ -3,7 +3,9 @@ import { NamedColor } from '../types/colorLocalisation';
 import { Keywords } from '../types/keywords';
 import { ProjectContext } from '../types/projectContext';
 import { ProjectPartnerContext } from '../types/projectPartnerContext';
+import { Technologies } from '../types/technologies';
 import { PLASMA } from './plasma';
+import { VIRIDIS } from './viridis';
 
 const getLightness = (color: [number, number, number]): number => 0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2];
 
@@ -18,11 +20,24 @@ const getColorFromUUIDPlasma = (t: number): [number, number, number] => {
   return [PLASMA[i0][0] * (1 - f) + PLASMA[i1][0] * f, PLASMA[i0][1] * (1 - f) + PLASMA[i1][1] * f, PLASMA[i0][2] * (1 - f) + PLASMA[i1][2] * f];
 };
 
-const missingAttributes: (Keywords | ProjectContext | ProjectPartnerContext)[] = [
+// helper method for getting a color for a given t value (between 0 and 1)
+const getColorFromUUIDViridis = (t: number): [number, number, number] => {
+  const index = t * (VIRIDIS.length - 1);
+  const i0 = Math.floor(index);
+  const i1 = Math.ceil(index);
+
+  if (i0 === i1) return VIRIDIS[i0];
+  const f = index - i0;
+  return [VIRIDIS[i0][0] * (1 - f) + VIRIDIS[i1][0] * f, VIRIDIS[i0][1] * (1 - f) + VIRIDIS[i1][1] * f, VIRIDIS[i0][2] * (1 - f) + VIRIDIS[i1][2] * f];
+};
+
+const missingAttributesPlasma: (Keywords | ProjectContext | ProjectPartnerContext)[] = [
   ...Object.values(Keywords),
   ...Object.values(ProjectContext),
   ...Object.values(ProjectPartnerContext),
 ];
+
+const missingAttributesViridis: Technologies[] = [...Object.values(Technologies)];
 
 export const PrimaryColorMap: Record<NamedColor, string> = {
   [ProjectCategory.Design]: 'white',
@@ -30,9 +45,15 @@ export const PrimaryColorMap: Record<NamedColor, string> = {
   [ProjectCategory.Art]: 'white',
   [ProjectCategory.Urbanism]: 'red',
   ...Object.fromEntries(
-    missingAttributes.map((attributeName, index, array) => [
+    missingAttributesPlasma.map((attributeName, index, array) => [
       attributeName,
       getLightness(getColorFromUUIDPlasma(index / (array.length - 1))) < 0.5 ? 'white' : 'black',
+    ])
+  ),
+  ...Object.fromEntries(
+    missingAttributesViridis.map((attributeName, index, array) => [
+      attributeName,
+      getLightness(getColorFromUUIDViridis(index / (array.length - 1))) < 0.5 ? 'white' : 'black',
     ])
   ),
 } as Record<NamedColor, string>;
@@ -43,9 +64,17 @@ export const SecondaryColorMap: Record<NamedColor, string> = {
   [ProjectCategory.Art]: 'blue',
   [ProjectCategory.Urbanism]: 'black',
   ...Object.fromEntries(
-    missingAttributes.map((attributeName, index, array) => [
+    missingAttributesPlasma.map((attributeName, index, array) => [
       attributeName,
       `rgb(${getColorFromUUIDPlasma(index / (array.length - 1))
+        .map((v) => ((1 + v) % 1) * 255)
+        .join(',')})`,
+    ])
+  ),
+  ...Object.fromEntries(
+    missingAttributesViridis.map((attributeName, index, array) => [
+      attributeName,
+      `rgb(${getColorFromUUIDViridis(index / (array.length - 1))
         .map((v) => ((1 + v) % 1) * 255)
         .join(',')})`,
     ])
