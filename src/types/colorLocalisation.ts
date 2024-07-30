f3aaab732f021900aff00451c65341d14f123cf8
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-07-30 23:09:00 +0200

    added techologies to color localisation

--- src/types/colorLocalisation.ts
+++ src/types/colorLocalisation.ts
@@ -2,5 +2,6 @@ import { ProjectCategory } from './categoryTypes';
 import { Keywords } from './keywords';
 import { ProjectContext } from './projectContext';
 import { ProjectPartnerContext } from './projectPartnerContext';
+import { Technologies } from './technologies';
 
-export type NamedColor = ProjectCategory | Keywords | ProjectContext | ProjectPartnerContext;
+export type NamedColor = ProjectCategory | Keywords | ProjectContext | ProjectPartnerContext | Technologies;
