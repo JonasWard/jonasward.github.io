0f803d12440a6ca84c705d783b16feece4a8a53d
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-09-06 18:15:12 +0200

    whole bunch of styling tweaks

---
+++ src/projects/projectComponents/ProjectExternalLinkRenderer.tsx
@@ -0,0 +1,9 @@
+import { ProjectExternalLink } from '../../types/projectContent/projectExternalLink';
+import './projectContent.css';
+
+export const ProjectExternalLinkRenderer: React.FC<{ content: ProjectExternalLink }> = ({ content }) => (
+  <div className='text-block link-block'>
+    {content.description ? <p>{content.description}</p> : null}
+    <a href={content.href}>{content.alternativeName ?? content.href}</a>
+  </div>
+);
