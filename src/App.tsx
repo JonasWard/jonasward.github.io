8ed6f25cf6c0e08079331f8423f77556fe9fcf8b
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-09-06 19:04:05 +0200

    removed app component

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import './App.css';
-import './assets/fonts/Montserrat-VariableFont_wght.ttf';
-
-const App = () => {
-  return <div>Well there should be something here no ?</div>;
-};
-
-export default App;
