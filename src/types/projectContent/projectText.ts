6688158cddda8cba3988ddddba351ff3e1e60724
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-09-14 23:31:05 +0200

    new project text type

--- src/types/projectContent/projectText.ts
+++ src/types/projectContent/projectText.ts
@@ -3,5 +3,5 @@ import { ProjectContentType } from './projectContentType';
 export type ProjectText = {
   maxColumnCount: 1 | 2 | 3;
   type: ProjectContentType.Text;
-  content: [string, string | undefined][];
+  content: (string | [string, string])[];
 };
