e8ad7e3b6b3bedc1ba292a0d84efd8a395bdc181
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-10-02 21:25:03 +0200

    changed display size of two images

--- src/data/projects/urbanism/napolitSotterana.ts
+++ src/data/projects/urbanism/napolitSotterana.ts
@@ -41,17 +41,15 @@ export const napoliSotterana: ProjectData = {
     ]),
     {
       type: ProjectContentType.ImageGrid,
-      images: [imageCortile, imageMonastery].map((i) => createImage(i, '©️ J.W.')),
+      images: [createImage(imageCortile, 'Cortile in Napoli'), createImage(imageMonastery, 'View of Monastery Courtyard in the inner city')],
     },
-    createImage(imageDecumani, '©️ J.W.'),
+    createImage(imageDecumani, 'View on the Decumani'),
     createText(
       2,
       'Is it at all possible to make these spaces part of the public domain? What happens when you start opening existing doors? Or can you create a secondary circulation scheme by just making a few holes in some walls? To visualize this I drew two maps. The first one is an up-to-date version of the Duca Di Noja (Napolitan Nolli Plan). This already sheds a wholly different light on the city. What seems to be impenetrable housing blocks actually manifest a sponge-like porosity. However, this map in itself doesn’t tell the whole story. What happens if you start coloring all the directly connected private and semi-private spaces is a similar fashion as you would color a world map? It clearly shows how interlinked the interiors of the blocks actually already are. On many occasions these internal passageways are already connected to streets on either side of the block, though just not publicly accessible.'
     ),
-    {
-      type: ProjectContentType.ImageGrid,
-      images: [mapDucaDiNoja, mapIleDansLesIlos].map((i) => createImage(i, '©️ J.W.')),
-    },
+    createImage(mapDucaDiNoja, 'Modern Duca Di Noja Map'),
+    createImage(mapIleDansLesIlos, 'Les Iles dans les Ilots Map'),
     createText(
       1,
       'The final map is a simple proof of concept to show how you can create a whole secondary circulation scheme with very limited interventions by interpreting the two maps.'
