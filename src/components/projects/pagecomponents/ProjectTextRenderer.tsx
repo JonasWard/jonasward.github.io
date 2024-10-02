78aa5033abfffe711cc6bbbda4f5d983c8794898
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-10-02 17:48:40 +0200

    simplified text renderer

--- src/components/projects/pagecomponents/ProjectTextRenderer.tsx
+++ src/components/projects/pagecomponents/ProjectTextRenderer.tsx
@@ -1,19 +1,17 @@
 import { ProjectText } from '../../../types/projectContent/projectText';
 import './projectContent.css';
 
-export const ProjectTextRenderer: React.FC<{ content: ProjectText }> = ({ content }) => {
-  return (
-    <div className={`text-block columns-${content.maxColumnCount}`}>
-      {content.content.map((content, index) =>
-        Array.isArray(content) ? (
-          <>
-            <h2 id={`${index}-title`}>{content[0]}</h2>
-            <p id={`${index}-p`}>{content[1]}</p>
-          </>
-        ) : (
-          <p id={`${index}`}>{content}</p>
-        )
-      )}
-    </div>
-  );
-};
+export const ProjectTextRenderer: React.FC<{ content: ProjectText }> = ({ content }) => (
+  <div className={`text-block columns-${content.maxColumnCount}`}>
+    {content.content.map((content, index) =>
+      Array.isArray(content) ? (
+        <>
+          <h2 key={`${index}-title`}>{content[0]}</h2>
+          <p key={`${index}-p`}>{content[1]}</p>
+        </>
+      ) : (
+        <p key={`${index}`}>{content}</p>
+      )
+    )}
+  </div>
+);
