d71077024f1ed3106a89e7866844ccef9e4f7e98
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-06-08 12:37:43 +0200

    feat: cv filename same logic as for motivation letter

--- src/components/cv/CV.tsx
+++ src/components/cv/CV.tsx
@@ -4,16 +4,14 @@ import { CVDocument } from './CVDocument';
 import { CVHTML } from './CVDigital';
 import printer from 'src/assets/icons/printer.svg';
 import { CVContent } from './content/cvContent';
+import { getFilenameString } from '../motivationLetter/motivationLetterDefaultContent';
 
 export const CV = () => {
   const date = new Date();
 
   return (
     <div className="cv-content">
-      <PDFDownloadLink
-        fileName={`JonasWard_CV_${date.getFullYear()}-${date.getMonth()}-${date.getDay()}.pdf`}
-        document={<CVDocument data={CVContent} />}
-      >
+      <PDFDownloadLink fileName={getFilenameString('CV')} document={<CVDocument data={CVContent} />}>
         <div className="cv-download-link">
           <img src={printer} alt="red-dot" />
           <span>Download</span>
