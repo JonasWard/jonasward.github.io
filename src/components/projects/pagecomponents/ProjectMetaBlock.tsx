cd5caeffabcad0ab557f2b2f81826b5d3211c2b9
Jonas Ward <jonas.vandenbulcke@gmail.com>
2026-03-15 17:25:54 +0100

    feat: color on project page

--- src/components/projects/pagecomponents/ProjectMetaBlock.tsx
+++ src/components/projects/pagecomponents/ProjectMetaBlock.tsx
@@ -17,49 +17,51 @@ export const ProjectMetaBlock: React.FC<IProjectMetaBlockProps> = ({ metaData })
   };
 
   return (
-    <div className="project-meta-block">
-      <div className="meta-item">
-        <span className="meta-label">category</span>
-        <button className="meta-value meta-value--link" onClick={() => goToFilter(metaData.projectType)}>
-          {metaData.projectType}
-        </button>
-      </div>
-      <div className="meta-item">
-        <span className="meta-label">context</span>
-        <button className="meta-value meta-value--link" onClick={() => goToFilter(metaData.projectContext)}>
-          {metaData.projectContext}
-        </button>
-      </div>
-      <div className="meta-item">
-        <span className="meta-label">collaboration</span>
-        <button className="meta-value meta-value--link" onClick={() => goToFilter(metaData.projectPartnerContext)}>
-          {metaData.projectPartnerContext}
-        </button>
-      </div>
-      {metaData.projectPartners && metaData.projectPartners.length > 0 && (
+    <div className={`project-meta-block-left ${metaData.projectType}`}>
+      <div className="project-meta-block">
         <div className="meta-item">
-          <span className="meta-label">partners</span>
-          <div className="meta-partners">
-            {metaData.projectPartners.map((p, i) => (
-              <button key={i} className="meta-value meta-value--link" onClick={() => goToFilter(p)}>
-                {p}
-              </button>
-            ))}
-          </div>
+          <span className="meta-label">category</span>
+          <button className="meta-value meta-value--link" onClick={() => goToFilter(metaData.projectType)}>
+            {metaData.projectType}
+          </button>
         </div>
-      )}
-      {metaData.keywords && metaData.keywords.length > 0 && (
-        <div className="meta-item meta-item--keywords">
-          <span className="meta-label">keywords</span>
-          <div className="meta-keywords">
-            {metaData.keywords.map((kw, i) => (
-              <button key={i} className="meta-keyword" onClick={() => goToFilter(String(kw))}>
-                {kw}
-              </button>
-            ))}
-          </div>
+        <div className="meta-item">
+          <span className="meta-label">context</span>
+          <button className="meta-value meta-value--link" onClick={() => goToFilter(metaData.projectContext)}>
+            {metaData.projectContext}
+          </button>
         </div>
-      )}
+        <div className="meta-item">
+          <span className="meta-label">collaboration</span>
+          <button className="meta-value meta-value--link" onClick={() => goToFilter(metaData.projectPartnerContext)}>
+            {metaData.projectPartnerContext}
+          </button>
+        </div>
+        {metaData.projectPartners && metaData.projectPartners.length > 0 && (
+          <div className="meta-item">
+            <span className="meta-label">partners</span>
+            <div className="meta-partners">
+              {metaData.projectPartners.map((p, i) => (
+                <button key={i} className="meta-value meta-value--link" onClick={() => goToFilter(p)}>
+                  {p}
+                </button>
+              ))}
+            </div>
+          </div>
+        )}
+        {metaData.keywords && metaData.keywords.length > 0 && (
+          <div className="meta-item meta-item--keywords">
+            <span className="meta-label">keywords</span>
+            <div className="meta-keywords">
+              {metaData.keywords.map((kw, i) => (
+                <button key={i} className="meta-keyword" onClick={() => goToFilter(String(kw))}>
+                  {kw}
+                </button>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
