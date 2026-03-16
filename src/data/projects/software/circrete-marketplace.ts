38a1581d7973bc6a0e02d8caf70c3b49a9853765
Jonas Ward <jonas.vandenbulcke@gmail.com>
2026-03-16 10:58:40 +0100

    feat: circrete tool & marketplace

---
+++ src/data/projects/software/circrete-marketplace.ts
@@ -0,0 +1,54 @@
+import { ProjectPartnerContext } from '../../../types/keywords/projectPartnerContext';
+import { ProjectContext } from '../../../types/keywords/projectContext';
+import { ProjectMetaData } from '../../../types/projectContent/projectMetaData';
+import { ProjectCategory } from '../../../types/keywords/categoryTypes';
+import { ProjectData } from '../../../types/projectContent/projectData';
+import { createImage, createText, createTitleImage } from '../../../utils/projectconstructor';
+import { Keywords } from '../../../types/keywords/keywords';
+import { ProjectContentType } from '../../../types/projectContent/projectContentType';
+
+import tinderSliding from './assets/circrete-marketplace/tinder-sliding.webp';
+import favorites from './assets/circrete-marketplace/favorites.webp';
+import multiLingual from './assets/circrete-marketplace/multi-lingual.webp';
+import overviewElements from './assets/circrete-marketplace/overview-elements.webp';
+import swiping from './assets/circrete-marketplace/swiping.webp';
+
+const id = '2025-06';
+
+const metaData: ProjectMetaData = {
+  id,
+  webstring: 'circrete-marketplace',
+  name: 'circrete Marketplace',
+  projectType: ProjectCategory.Software,
+  description: 'POC Marketplace for circrete Concrete Elements',
+  keyImage: overviewElements,
+  projectContext: ProjectContext.Personal,
+  projectPartnerContext: ProjectPartnerContext.Solo,
+  keywords: [Keywords.Frontend, Keywords.Development, Keywords.Ecology, Keywords.Reuse]
+};
+
+export const circreteMarketplace: ProjectData = {
+  id,
+  metaData,
+  projectImage: createTitleImage(overviewElements, metaData.name, 3024, 2634),
+  projectContent: [
+    createText(
+      2,
+      [
+        'Giving Structural Elements a place to find their new Home',
+        'circrete Marketplace serves as a POC for a marketplace for Structural Elements. Using different interfaces, users can get an overview of the available elements to use in their projects. There is quite some fuzziness.'
+      ],
+      'This project is a static frontend page, using next.js, while also allowing for multiple languages.'
+    ),
+    {
+      type: ProjectContentType.ImageGrid,
+      images: [
+        createImage(favorites, 'Favorites'),
+        createImage(multiLingual, 'Multi-lingual'),
+        createImage(overviewElements, 'Overview'),
+        createImage(tinderSliding, 'Tinder Sliding'),
+        createImage(swiping, 'Swiping')
+      ]
+    }
+  ]
+};
---
+++ src/data/projects/software/circrete-tool.ts
@@ -0,0 +1,45 @@
+import { ProjectPartnerContext } from '../../../types/keywords/projectPartnerContext';
+import { ProjectContext } from '../../../types/keywords/projectContext';
+import { ProjectMetaData } from '../../../types/projectContent/projectMetaData';
+import { ProjectCategory } from '../../../types/keywords/categoryTypes';
+import { ProjectData } from '../../../types/projectContent/projectData';
+import { createImage, createText, createTitleImage } from '../../../utils/projectconstructor';
+import { Keywords } from '../../../types/keywords/keywords';
+import { ProjectContentType } from '../../../types/projectContent/projectContentType';
+
+import view from './assets/circrete-tool/3d-view.webp';
+import components from './assets/circrete-tool/components.webp';
+import geometry from './assets/circrete-tool/geometry.webp';
+import overview from './assets/circrete-tool/overview.webp';
+
+const id = '2025-04';
+
+const metaData: ProjectMetaData = {
+  id,
+  webstring: 'circrete-tool',
+  name: 'circrete Tool',
+  projectType: ProjectCategory.Software,
+  description: 'circrete Assesment tool for Concrete Elements',
+  keyImage: view,
+  projectContext: ProjectContext.Professional,
+  projectPartnerContext: ProjectPartnerContext.Team,
+  keywords: [Keywords.Frontend, Keywords.Development, Keywords.Ecology, Keywords.Reuse],
+  projectPartners: ['Janin Brandt']
+};
+
+export const circreteTool: ProjectData = {
+  id,
+  metaData,
+  projectImage: createTitleImage(view, metaData.name, 1761, 1375),
+  projectContent: [
+    createText(2, ['POC Assesment Tool', 'Static webpage, with convex backend, written in typescript.']),
+    {
+      type: ProjectContentType.ImageGrid,
+      images: [
+        createImage(components, 'Component List View'),
+        createImage(geometry, 'Geometry level View'),
+        createImage(overview, 'Overview')
+      ]
+    }
+  ]
+};
