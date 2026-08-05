178cf4b3a321c3e7e34aff89a8a4bdc7fdc88724
Jonas Ward <jonas.vandenbulcke@gmail.com>
2026-08-05 23:21:56 +0200

    feat: motivation letter styling

--- src/components/motivationLetter/MotivationLetterPDF.tsx
+++ src/components/motivationLetter/MotivationLetterPDF.tsx
@@ -23,6 +23,10 @@ const months = [
   'December'
 ];
 
+const contentStyle = {
+  fontSize: 13
+};
+
 export const MotivationPDF: React.FC<{ motivationContent: MotivationLetterContent }> = ({ motivationContent }) => (
   <Document title={getFilenameString('Motivation_Letter')}>
     <Page dpi={90} size="A4" style={REACT_PDF_STYLES.page}>
@@ -55,24 +59,31 @@ export const MotivationPDF: React.FC<{ motivationContent: MotivationLetterConten
         </div>
       </div>
       <div id={'right'} style={{ ...REACT_PDF_STYLES.left, width: '100%', paddingLeft: 0, fontSize: 15 }}>
-        <div id={'education'} style={{ ...REACT_PDF_STYLES.block, textAlign: 'left', gap: 10 }}>
+        <div id={'education'} style={{ ...REACT_PDF_STYLES.block, textAlign: 'left' }}>
           <TitleRenderer title={motivationContent.title} isPdf />
-          <PDFDivText isPdf content={motivationContent.intro} />
-          <PDFDivText isPdf content={motivationContent.people} />
-          {motivationContent.content?.split('\n').map((line, i) => (
-            <PDFDivText isPdf content={line} />
-          ))}
-          <DividingSpace id={'1'} />
-          <PDFDivText isPdf content={motivationContent.outro} />
-          <PDFDivText isPdf content={`${motivationContent.wishes}`} />
-          <PDFDivText isPdf content={`${`${CVContent.info.firstName.split(' ')[0]} ${CVContent.info.name}`}`} />
-          <DividingSpace id={'2'} />
-          <PDFDivText
-            isPdf
-            content={`${motivationContent.city}, ${new Date().getDate()} ${
-              months[new Date().getMonth()]
-            } ${new Date().getFullYear()}`}
-          />
+          <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+            <PDFDivText isPdf content={motivationContent.intro} style={REACT_PDF_STYLES.tagTitle} />
+            <PDFDivText isPdf content={motivationContent.people} style={contentStyle} />
+            {motivationContent.content?.split('\n').map((line, i) => (
+              <PDFDivText isPdf content={line} style={contentStyle} />
+            ))}
+            <DividingSpace id={'1'} />
+            <PDFDivText isPdf content={motivationContent.outro} style={contentStyle} />
+            <PDFDivText isPdf content={`${motivationContent.wishes}`} style={contentStyle} />
+            <PDFDivText
+              isPdf
+              content={`${`${CVContent.info.firstName.split(' ')[0]} ${CVContent.info.name}`}`}
+              style={contentStyle}
+            />
+            <DividingSpace id={'2'} />
+            <PDFDivText
+              isPdf
+              content={`${motivationContent.city}, ${new Date().getDate()} ${
+                months[new Date().getMonth()]
+              } ${new Date().getFullYear()}`}
+              style={contentStyle}
+            />
+          </div>
         </div>
       </div>
     </Page>
